1da2cd14bae0490cfc03b02d1bee9f3631c19bae
Ashish Kumar <entropy.ash@gmail.com>
2018-08-30 21:31:05 +0530

    Some improvements in index

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 
-gulp.task('styles', function() {
-    console.log('heyyy');
-    return gulp.src('./sass/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css/'));
+gulp.task('styles', function () {
+	return gulp.src('./scss/*.scss')
+		.pipe(sass()
+			.on('error', sass.logError))
+		.pipe(gulp.dest('./css/'));
 });
 
-gulp.task('default',function() {
-    gulp.watch('./sass/*.scss',['styles']);
+gulp.task('default', function () {
+	gulp.watch('./scss/*.scss', ['styles']);
 });
