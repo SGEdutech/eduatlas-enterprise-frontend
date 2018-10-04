aa8ee07dc697f2d800ad4731832ba572a676a106
Ashish Kumar <entropy.ash@gmail.com>
2018-10-05 00:59:31 +0530

    Initialised new module

---
+++ assets/js/new-institute-fetch.js
@@ -0,0 +1,13 @@
+(() => {
+	let claimsArr;
+
+	function getClaimedInstitute() {
+
+	}
+
+	function init(claimsArray) {
+		if (claimsArr === undefined) throw new Error('Claim array not provided');
+
+		claimsArr = claimsArray;
+	}
+})();
