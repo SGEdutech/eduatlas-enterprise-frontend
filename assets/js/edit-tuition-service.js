af6d8f168d241116e209e6f0c76d25c97898792c
RoBo_ <mannvender001@gmail.com>
2018-10-10 21:07:35 +0530

    Fix: edit tuition, school

--- assets/js/edit-tuition-service.js
+++ assets/js/edit-tuition-service.js
@@ -1,27 +1,28 @@
-PubSub.subscribe('user', (msg, userInfo) => {
-    navigationBar.render(userInfo);
-    redirectOnLogout.init(userInfo);
-});
+try {
+	PubSub.subscribe('user', (msg, userInfo) => {
+		navigationBar.render(userInfo);
+		redirectOnLogout.init(userInfo);
+	});
 
-PubSub.subscribeOnce('query', (msg, queryObj) => {
-	redirectTabs.init(queryObj);
-});
+	PubSub.subscribeOnce('query', (msg, queryObject) => {
+		getDetails.returnData('tuition', queryObject).then((tuitionInfo) => {
+            redirectTabs.init(queryObject);
+			// coverImage.init('tuition', tuitionInfo);
+			basicDetails.init('tuition', tuitionInfo);
+			facilitiesDescriptionCategory.init('tuition', tuitionInfo);
+			contactUs.init('tuition', tuitionInfo);
+			// courses.init(tuitionInfo);
+			results.init('tuition', tuitionInfo);
+			faculty.init('tuition', tuitionInfo);
+			galleryTab.init('tuition', tuitionInfo);
+		});
+	});
 
-PubSub.subscribeOnce('query.load', (msg, queryObject) => {
-    getDetails.returnData('tuition', queryObject).then((tuitionInfo) => {
-        // coverImage.init('tuition', tuitionInfo);
-        basicDetails.init('tuition', tuitionInfo);
-        facilitiesDescriptionCategory.init('tuition', tuitionInfo);
-        contactUs.init('tuition', tuitionInfo);
-        // courses.init(tuitionInfo);
-        results.init('tuition', tuitionInfo);
-        faculty.init('tuition', tuitionInfo);
-        galleryTab.init('tuition', tuitionInfo);
-    });
-});
+	user.getInfo().then(userInfo => {
+		navigationBar.init(userInfo);
+	});
 
-user.getInfo().then(userInfo => {
-    navigationBar.init(userInfo);
-});
-
-queryString.loadQueryString();
+	queryString.loadQueryString();
+} catch (error) {
+	console.error(error);
+}
