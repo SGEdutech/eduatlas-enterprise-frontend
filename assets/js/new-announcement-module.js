d6ec9bedda7fa9ea76c14674e57014e2c86a9646
Ashish Kumar <entropy.ash@gmail.com>
2018-11-26 01:46:17 +0530

    Fix: Adding new announcement throwing error when no student is selected

--- assets/js/new-announcement-module.js
+++ assets/js/new-announcement-module.js
@@ -19,7 +19,12 @@ const announcement = (() => {
 		const tuitionId = $button.attr('data-tuition-id');
 
 		const userEmails = [];
-		$studentCheckbox.filter(`[data-tuition-id="${tuitionId}"]:checked`).each((__, inp) => {
+		const $checkedStudentCheckbox = $studentCheckbox.filter(`[data-tuition-id="${tuitionId}"]:checked`);
+		if ($checkedStudentCheckbox.length === 0) {
+			alert('Please select atleast one student to send notification to');
+			return;
+		}
+		$checkedStudentCheckbox.each((__, inp) => {
 			const $inp = $(inp);
 			userEmails.push($inp.val());
 		});
